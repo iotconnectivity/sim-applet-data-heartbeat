01a24a22c6b1563a2affbbb3ce4113fa5a4b527a
Diego Leñero Ramírez <dielenram@gmail.com>
2018-10-16 19:15:03 +0200

    First version of data heartbeat applet

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # sim-applet-data-heartbeat
 JavaCard SIM card applet to monitor the status of data connectivity over-the-air
+
+## Table of Contents
+
+- [Description](#description)
+- [To Do](#todo)
+- [Installation and usage](#installation)
+- [Contributing and license](#contributing)
+
+
+## Description
+
+The purpouse of this applet is to send a heartbeat message throught data connectivity. [ETSI TS 143 019 V5.6.0 Annex D](https://www.etsi.org/deliver/etsi_ts/143000_143099/143019/05.06.00_60/ts_143019v050600p.pdf) shows an example of the proactive command OPEN CHANNEL. Here we try to use that example. 
+
+
+## ToDo
+
+- To find information to configure the OPEN CHANNEL parameters
+
+## Installation
+
+To install the applet you need:
+- sysmoUSIM-SJS1 SIM + USIM Card with ADM1 key (find it here: http://shop.sysmocom.de/products/sysmousim-sjs1).
+- a card reader.
+- The scripts for installation are in the osmocom [repository](http://git.osmocom.org/sim/sim-tools/) and you have a guide [here](https://osmocom.org/projects/sim-toolkit/wiki)
+
+## Contributing
+
+Please contribute using [Github Flow](https://guides.github.com/introduction/flow/). Create a branch, add commits, and [open a pull request](https://github.com/fraction/readme-boilerplate/compare/).
+
+Please note this source code has been released under the GPLv3 terms and all contributions will be considered. Have a look at the LICENSE file distributed with this code.
---
+++ src/Heartbeat/Heartbeat.java
@@ -0,0 +1,214 @@
+/**
+ *  	Heartbeat applet
+ *
+ * 		Author: Diego Leñero Ramírez 
+ *				diego.lenero@podgroup.com
+ *		 
+ */
+
+
+package Heartbeat;
+
+import sim.toolkit.*;
+import sim.access.*;
+import javacard.framework.*;
+
+public class Heartbeat extends Applet implements ToolkitInterface, ToolkitConstants {
+
+	static 	byte[] 	menuItemText = new byte[] {'H','e','a','r','t','b','e','a','t'};
+	private byte 	menuItem;
+    private ToolkitRegistry reg;
+
+    private static byte[] helloWorld = new byte[] {'H','e','l','l','o','W','o','r','l','d'};
+    private byte[] data;        // buffer 
+
+
+    // constants to manage nibbles
+    public static final byte NIBBLE_SIZE  = (byte)4;
+    public static final byte UPPER_NIBBLE  = (byte)0xF0;
+    public static final byte LOWER_NIBBLE  = (byte)0x0F;
+
+    
+
+    // BIP Constants
+    private static final byte TAG_DATA_DESTINATION_ADDRESS           = (byte) 0x3E;
+
+    // Bearer variables
+    // --> GPRS case...
+    
+    // The APN is a reference to a GGSN: myoperator.mycountry
+    // This value is coded in a special way: LVLV...
+    private static byte[] myAPN = {(byte)0x0A,'d','a','t','a','6','4','1','0','0','3'};
+    
+                
+
+    //GPRS = 0x02
+    private static final byte BEARER_TYPE_GPRS                       = (byte) 0x02; 
+
+    // The service precedence class indicates the relative priority of maintaining the service
+    //Precedence Class nº 1 => High priority.
+    private static final byte BEARER_PARAMETER_PRECEDENCE_CLASS1     = (byte) 0x01;
+    
+    // The delay parameter defines the end-to-end transfer delay incurred in the transmission of the SDUs through 
+    // the GPRS network
+    //Delay Class n°1 => Predictive 
+    private static final byte BEARER_PARAMETER_DELAY_CLASS1          = (byte) 0x01;
+
+    // The reliability parameter indicates the transmission characteristics that are required by an application. 
+    // The reliability class defines the probability of loss of, duplication of, 
+    // mis sequencing of or corruption of SDUs
+    private static final byte BEARER_PARAMETER_RELIABILITY_CLASS1    = (byte) 0x01;
+
+    // The peak throughput class specifies the maximum rate at which data is expected to be transferred accross
+    // the network for an individual PDP context.
+    //Peak throughput in octets per second up to 1000 (8kbits/s)
+    private static final byte BEARER_PARAMETER_PEAK_THROUGHPUT_CLASS1= (byte) 0x01;
+    
+    // The Mean throughput class specifies the average rate at which data is expected to be transferred 
+    // accross the GPRS network during the remaining lifetime of an activated Packet Data Protocol (PDP) context.
+    //Mean throughput in octets per hour 100 (~0.22bits/s)
+    private static final byte BEARER_PARAMETER_MEAN_THROUGHPUT_CLASS1    = (byte) 0x01;
+
+    // PDP Type --> IP
+    private static final byte BEARER_PARAMETER_PDP_IP                    = (byte) 0x02;
+
+    // Define parameters necessary to launch a PDP context
+    private static byte[] BearerParameters=    {(byte) BEARER_TYPE_GPRS, (byte) BEARER_PARAMETER_PRECEDENCE_CLASS1,
+        (byte) BEARER_PARAMETER_DELAY_CLASS1, (byte) BEARER_PARAMETER_RELIABILITY_CLASS1,
+        (byte) BEARER_PARAMETER_PEAK_THROUGHPUT_CLASS1, (byte) BEARER_PARAMETER_MEAN_THROUGHPUT_CLASS1,
+        (byte) BEARER_PARAMETER_PDP_IP };
+
+
+    // Type of Address IPV4=21,
+    private static final byte TYPE_OF_ADDRESS_IPV4      = (byte) 0x21;
+    // SIM/ME interface transport level UDP=01
+    private static final byte TRANSPORT_PROTOCOL_TYPE   = (byte) 0x01;
+
+    // --> ... end GPRS case
+
+    private byte result;
+
+     // Allocate 500 bytes for the reception buffer
+    private static final short BUFFER_SIZE = (short)0x01F4;  
+    // Maximum response handler capacity to be used for the receive data 
+    private static short myRspHdlrCapacity;
+
+    // Port number : integer
+    private static byte[] portNumber = {(byte)0x00,(byte)0x23};
+    // IP Address of the Server 
+    private static byte[] IPAddress = {(byte)163,(byte)187,(byte)203,(byte)1};
+    // Channel Identifier given by the ME after the OPEN CHANNEL proactive command
+    private static byte channelID = 0x00;
+
+
+
+	/**
+	*	Constructor of the applet
+	*/
+	private Heartbeat() {
+		reg = ToolkitRegistry.getEntry(); // register to the SIM Toolkit Framework
+		//reg.setEvent(event...);
+
+		// Define the applet Menu Entry
+		menuItem = reg.initMenuEntry(menuItemText, (short)0, (short)menuItemText.length,
+				PRO_CMD_SELECT_ITEM, false, (byte)0, (short)0);
+        data        = JCSystem.makeTransientByteArray ((short)2, JCSystem.CLEAR_ON_RESET);
+	}
+
+	/**
+    * Method called by the JCRE at the installation of the applet
+    */
+	public static void install(byte[] bArray, short bOffset, byte bLength) {
+		Heartbeat applet = new Heartbeat();
+		applet.register();
+	}
+
+
+	public void process(APDU apdu) throws ISOException {
+		if (selectingApplet())
+		return;
+	}
+	
+    /**
+    * Method called by the SIM Toolkit Framework
+    */
+	public void processToolkit(byte event) throws ToolkitException {
+		EnvelopeHandler		envHdlr = EnvelopeHandler.getTheHandler();
+        ProactiveHandler	proHdlr = ProactiveHandler.getTheHandler();
+        ProactiveResponseHandler rspHdlr ;
+
+		if (event == EVENT_MENU_SELECTION) {
+			byte selectedItemId = envHdlr.getItemIdentifier();
+
+			if (selectedItemId == menuItem) {
+
+                try {
+    				// 1st Open GPRS connection
+    				// prepare the OPEN CHANNEL (on demand link establishment = 0x00 for the qualifier byte)
+    				proHdlr.init(PRO_CMD_OPEN_CHANNEL,(byte)0, DEV_ID_ME);
+    				// Add Bearer description TLV
+    				proHdlr.appendTLV((byte)(TAG_BEARER_DESCRIPTION | TAG_SET_CR),(byte) BEARER_TYPE_GPRS, 
+    					BearerParameters,(short)0,(short)BearerParameters.length);
+                    // Add Buffer size TLV
+                    proHdlr.appendTLV((byte)(TAG_BUFFER_SIZE | TAG_SET_CR),(byte)(BUFFER_SIZE>>(byte)8),(byte)BUFFER_SIZE);
+                    // Add APN TLV
+                    proHdlr.appendTLV((byte)(TAG_NETWORK_ACCESS_NAME | TAG_SET_CR),myAPN,(short)0,(short)myAPN.length);
+                    // Add SIM/ME interface transport level UDP=01, Port 23 = Telnet
+                    proHdlr.appendTLV((byte) (TAG_SIM_ME_INTERFACE_TRANSPORT_LEVEL | TAG_SET_CR),TRANSPORT_PROTOCOL_TYPE, 
+                                        portNumber, (short)0, (short)portNumber.length);
+                    // Add Data Destination Address TLV
+                    // Type of Address IPV4=21, IPAddress contains the IPV4 Address
+                    proHdlr.appendTLV((byte) (TAG_DATA_DESTINATION_ADDRESS | TAG_SET_CR),TYPE_OF_ADDRESS_IPV4, 
+                                        IPAddress,(short)0, (short) IPAddress.length);
+
+
+
+
+
+                    result = proHdlr.send();
+
+
+
+/*
+                    // 2° Retrieve the channel ID allocated by the ME 
+                    if (result == RES_CMD_PERF){
+                        rspHdlr = ProactiveResponseHandler.getTheHandler();
+                        channelID = rspHdlr.getChannelIdentifier();
+                        // Get the maximum size of the Simple TLV list managed by the response handler
+                        myRspHdlrCapacity = rspHdlr.getCapacity();
+            
+                    }
+                    else {
+                        channelID = 0x00;
+                    }
+
+
+                    proHdlr.init((byte)PRO_CMD_SEND_DATA,(byte)0,channelID);
+                    proHdlr.appendTLV(TAG_CHANNEL_DATA,helloWorld,(short)0,(short)helloWorld.length);
+                    proHdlr.send();
+
+                    proHdlr.initCloseChannel(channelID);
+                    proHdlr.send();
+*/
+
+                }
+
+                /*catch (SIMViewException MyException){
+                    short reason = MyException.getReason();
+                    data[1]=(byte) reason;
+                    data[0]=(byte) (((reason & UPPER_NIBBLE) >> NIBBLE_SIZE)& LOWER_NIBBLE);
+                    proHdlr.initDisplayText((byte)0x80, DCS_8_BIT_DATA, data,(short)0, (short)2);
+                    proHdlr.send();
+                }*/
+
+                catch (ToolkitException MyException) {
+                    short reason = MyException.getReason();
+                    data[1]=(byte) reason;
+                    data[0]=(byte) (((reason & UPPER_NIBBLE) >> NIBBLE_SIZE)& LOWER_NIBBLE);
+                    proHdlr.initDisplayText((byte)0x80, DCS_8_BIT_DATA, data,(short)0, (short)2);
+                    proHdlr.send();
+                }
+			}
+		}
+	} 
+}
